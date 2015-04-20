a8f315b65a18a100afa3eb7948b16d6c8c006e36
Marius Cotofana <marius.coto@gmail.com>
2015-04-20 19:42:14 +0300

    Small fixes; add comments

--- routes/activities.js
+++ routes/activities.js
@@ -3,66 +3,70 @@ var Edition  = require('../config/models/activity').edition
 var mongoose = require('mongoose')
 var objId    = mongoose.Types.ObjectId
 
+
+// Activities page
 exports.index = function(req, res) {
   Activity.find().exec(gotActivities)
 
   function gotActivities(err, all) {
-      res.render('activities', {
-        'activities': all,
-        'user'      : req.session.user
-      })
+    res.render('activities', {
+      'activities': all,
+      'user'      : req.session.user
+    })
   }
 }
 
+// Single activity page
 exports.one = function(req, res) {
   var activityId = objId.fromString(req.params.activity)
   Activity.findOne({'_id': activityId}).exec(gotActivity)
 
   function gotActivity(err, one) {
-      res.render('activity', {
-        'activity'  : one,
-        'user'      : req.session.user
-      })
+    res.render('activity', {
+      'activity'  : one,
+      'user'      : req.session.user
+    })
   }
 }
 
+// Add a new activity handle
 exports.add = function(req, res) {
-  var new_activity = {
+  new Activity({
     'name'        : req.body.name,
     'description' : req.body.description
-  }
-
-  new Activity(new_activity).save(function(err) {
-    res.render('errors', {
-      'message': 'Failed to save new activity.',
-      'user':   req.user ? req.user.google : false
-    });
+  }).save(function(err) {
+    if (err) console.log('[ERR] Could not save activity.')
   })
+
   res.redirect('/activities')
 }
 
+// Add a new edition handle
 exports.add_edition = function(req, res) {
+  // Get dates and format them for js date object
   var start = req.body.start_date
+  var start_date = new Date(start.substring(7,11), start.substring(3,5), start.substring(0,2))
   var end = req.body.end_date
-  var new_edition = {
+  var end_date = new Date(end.substring(7,11), end.substring(3,5), end.substring(0,2))
+
+  // Create edition object
+  var newEdition = {
     'name'        : req.body.name,
-    'start'       : new Date(start.substring(7,11), start.substring(3,5), start.substring(0,2)),
-    'end'         : new Date(end.substring(7,11), end.substring(3,5), end.substring(0,2))
+    'start'       : start_date,
+    'end'         : end_date
   }
 
-  var newEdition = new Edition(new_edition)
-  newEdition.save(function(err) {
-    if(err) {
-      res.render('errors', {
-        'message': 'Failed to save new edition.',
-        'user':   req.user ? req.user.google : false
-      });
+  new Edition(newEdition).save(function (err) {
+    if (err) {
+      console.log('[ERR] Could not save edition.')
+
+    } else {
+      // Add edition to activity object
+      var find = {'_id': objId.fromString(req.params.activity)}
+      var update = {$push: {'edition': newEdition}}
+      Activity.update(find, update).exec()
     }
   })
-
-  var find = {'_id': objId.fromString(req.params.activity)}
-  var update = {$push: {'edition': newEdition}}
-  Activity.update(find, update).exec()
 
   res.redirect('/activities/' + req.params.activity)
 }
